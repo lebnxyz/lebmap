163fcd263cbdfce63dc38cd02902a2773f295f34
ht <eltrhn@gmail.com>
2019-09-23 02:48:58 -0700

    Draw Lebanon, finally

--- web/main.js
+++ web/main.js
@@ -1,4 +1,19 @@
-let map = d3.select('#map');
-let chart = d3.select('#chart');
+const width = 500;
+const height = 500;
 
-// ...
+const map = d3.select('#map')
+  .attr('width', width)
+  .attr('height', height);
+const chart = d3.select('#chart');
+
+d3.json('../data/map/lb_2009_administrative_districts.geojson').then(function(lb) {
+    const projection = d3.geoEqualEarth()
+      .fitSize([width, height], lb);
+    const path = d3.geoPath(projection);
+
+    map.append('path')
+      .attr('d', path(lb))
+      .attr('fill', 'black')
+      .attr('stroke', 'white');
+});
+
