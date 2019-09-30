817897d12e94615e679e3ccbacbcb26858c42d9c
ht <eltrhn@gmail.com>
2019-09-29 22:44:25 -0700

    Move svg-creation to js (via d3) to make Parcel happy
    This is probably better practice anyway so whatev. But Parcel for some reason removes the `id` attr of pre-defined SVG elements in index.html

--- assets/main.js
+++ assets/main.js
@@ -6,35 +6,38 @@ import * as d3 from 'd3';
 const width = 600;
 const height = 600;
 
-const mapSVG = d3.select('#map')
+
+const mapSVG = d3.select('body').append('svg')
+  .attr('id', 'map')
   .attr('width', width)
   .attr('height', height);
-const chartSVG = d3.select('#chart');
+const chartSVG = d3.select('body').append('svg')
+  .attr('id', 'chart');
 
 mapSVG.append('g').attr('id', 'path-group');
 mapSVG.append('g').attr('id', 'circle-group');
 
 Promise.all([
-  d3.json(mapJSON),
-  d3.json(locJSON)
+    d3.json(mapJSON),
+    d3.json(locJSON)
 ]).then(function([mapJSON, locJSON]) {
     const projection = customScaledProjection(1.1, 1, d3.geoMercatorRaw)
-      .fitSize([width, height], mapJSON);
+        .fitSize([width, height], mapJSON);
     const path = d3.geoPath(projection);
 
     mapSVG.select('#path-group').selectAll('path')
-      .data(mapJSON.features)
-      .enter()
-      .append('path')
-      .attr('d', path)
-      // class .hover rather pseudo :hover required because Firefox is lame
-      .on('mouseover', function() { d3.select(this).raise().classed('hover', true); })
-      .on('mouseout', function() { d3.select(this).lower().classed('hover', false); });
+        .data(mapJSON.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        // class .hover rather pseudo :hover required because Firefox is lame
+        .on('mouseover', function() { d3.select(this).raise().classed('hover', true); })
+        .on('mouseout', function() { d3.select(this).lower().classed('hover', false); });
     
     mapSVG.select('#circle-group').selectAll('circle')
-      .data(d3.values(locJSON), function(o) { return o.name; })
-      .enter()
-      .append('circle')
-      .attr('cx', function(o) { return projection(o.location)[0]; })
-      .attr('cy', function(o) { return projection(o.location)[1]; });
+        .data(d3.values(locJSON), function(o) { return o.name; })
+        .enter()
+        .append('circle')
+        .attr('cx', function(o) { return projection(o.location)[0]; })
+        .attr('cy', function(o) { return projection(o.location)[1]; });
 });
