309827a6392a1f59a81511974f5b9667cae75637
ht <eltrhn@gmail.com>
2019-10-05 01:49:11 -0700

    Change utils, attempt to create variable circle size, add alasql

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,10 @@
-import { customScaledProjection } from './scripts/d3-funcs.js';
+import { customScaledProjection } from './scripts/utils.js';
+import { countLocation } from './scripts/oneOffHelpers.js'
 import mapJSON from './data/map/lb_2009_administrative_districts.geojson';
 import { url as locJSON } from './data/map/locations.json';
 import { data as respondentsJSON } from './data/respondents.json';
 import * as d3 from 'd3';
+import alasql from 'alasql';
 const width = 600;
 const height = 600;
 
@@ -39,5 +41,6 @@ Promise.all([
         .enter()
         .append('circle')
         .attr('cx', function(o) { return projection(o.location)[0]; })
-        .attr('cy', function(o) { return projection(o.location)[1]; });
+        .attr('cy', function(o) { return projection(o.location)[1]; })
+        .attr('r', function(o) { console.log(o.name, countLocation(o.name, respondentsJSON, alasql)); });
 });
--- assets/scripts/d3-funcs.js
+++
@@ -1,8 +0,0 @@
-import * as d3 from 'd3';
-
-export function customScaledProjection(xMult, yMult, rawProjection) {
-    return d3.geoProjection(function(...args) {
-        const [x, y] = rawProjection(...args);
-        return [x * xMult, y * yMult];
-    });
-}
