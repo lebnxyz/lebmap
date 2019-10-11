b6b27d3da2bba201fe3181b8930080500a12953a
ht <eltrhn@gmail.com>
2019-10-10 18:49:21 -0700

    Formatting

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,11 @@
-import { customScaledProjection, makeQueryFunc, normalize } from './scripts/utils.js';
-import { countLocationNormalized } from './scripts/oneOffHelpers.js'
+import * as d3 from 'd3';
+
+import { customScaledProjection, makeQueryFunc } from './scripts/utils.js';
+import * as oneOff from './scripts/oneOffHelpers.js';
+
 import mapJSON from './data/map/lb_2009_administrative_districts.geojson';
 import { url as locJSON } from './data/map/locations.json';
 import { data as respondentsJSON } from './data/respondents.json';
-import * as d3 from 'd3';
 
 const SVG_WIDTH = 600;
 const SVG_HEIGHT = 600;
@@ -28,24 +30,24 @@ Promise.all([
     d3.json(locJSON)
 ]).then(function([mapJSON, locJSON]) {
     const projection = customScaledProjection(1.1, 1, d3.geoMercatorRaw)
-        .fitSize([SVG_WIDTH, SVG_HEIGHT], mapJSON);
+      .fitSize([SVG_WIDTH, SVG_HEIGHT], mapJSON);
     const path = d3.geoPath(projection);
 
     mapSVG.select('#path-group').selectAll('path')
-        .data(mapJSON.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .on('click', function() { const o = d3.select(this); o.classed('clicked', !o.classed('clicked')); })
-        // class .hover rather pseudo :hover required because Firefox is lame
-        .on('mouseover', function() { d3.select(this).raise().classed('hover', true); })
-        .on('mouseout', function() { d3.select(this).classed('hover', false); });
+      .data(mapJSON.features)
+      .enter()
+      .append('path')
+      .attr('d', path)
+      .on('click', function() { const o = d3.select(this); o.classed('clicked', !o.classed('clicked')); })
+      // class .hover rather pseudo :hover required because Firefox is lame
+      .on('mouseover', function() { d3.select(this).raise().classed('hover', true); })
+      .on('mouseout', function() { d3.select(this).classed('hover', false); });
     
     mapSVG.select('#circle-group').selectAll('circle')
-        .data(d3.values(locJSON), function(o) { return o.name; })
-        .enter()
-        .append('circle')
-        .attr('cx', place => projection(place.location)[0])
-        .attr('cy', place => projection(place.location)[1])
-        .attr('r', place => countLocationNormalized(place.name, respondents, MIN_RAD, MAX_RAD));
+      .data(d3.values(locJSON), function(o) { return o.name; })
+      .enter()
+      .append('circle')
+      .attr('cx', place => projection(place.location)[0])
+      .attr('cy', place => projection(place.location)[1])
+      .attr('r', place => oneOff.countLocationNormalized(place.name, respondents, MIN_RAD, MAX_RAD));
 });
