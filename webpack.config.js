57774c31e1214ac2d61a78914c5ce624d2b9e277
ht <eltrhn@gmail.com>
2019-10-28 15:43:10 -0700

    Add webpack config and map geojson, removed by mistake

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+    map: './src/data/map/lb_2009_administrative_districts.json',
+    locations: './src/data/map/locations.json',
+    respondents: './src/data/respondents.json'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: './'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(geo|topo)json$/,
+        use: [
+          'json-loader',
+          'loader'
+        ]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            plugins: [
+              '@babel/plugin-syntax-dynamic-import'
+            ],
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: 3
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+}
