7b4c95b4f000649539780e293bbff0dd53c939e1
ht <eltrhn@gmail.com>
2019-10-30 12:00:36 -0700

    Add answer-list, mistakenly removed

---
+++ src/components/answer-list.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{selection}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnswerList',
+  props: {
+    selection: Object
+  },
+};
+</script>
+
+<style scoped>
+div {
+  color: white;
+}
+</style>
